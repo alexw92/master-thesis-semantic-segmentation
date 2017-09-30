37e3755f9a8469c56ddf86335398ac3cbe528894
Alex Werthmann <werdy@gmx.net>
2017-09-30 14:00:04 +0200

    project cud cunn gtx1080

--- Python_TensorFlow/TF_Experiments/Variables.py
+++ Python_TensorFlow/TF_Experiments/Variables.py
@@ -10,33 +10,47 @@ W = tf.Variable([.3], tf.float32)
 b = tf.Variable([-.3], tf.float32)
 
 # Inputs and Outputs
-# x = in; y = out
+# x = in; y = out (expected output)
 x = tf.placeholder(tf.float32)
 
+# model output
 linear_model = W * x + b
 
 y = tf.placeholder(tf.float32)
 
-# Loss
+# Loss (calculated value - correct value)
 squared_delta = tf.square(linear_model - y)
 loss = tf.reduce_sum(squared_delta)
 
-# Optimize
+# Optimizer
 optimizer = tf.train.GradientDescentOptimizer(0.01)
 train = optimizer.minimize(loss)
 
-# Define vars
+# Create var initializer
 init = tf.global_variables_initializer()
 
 # Create session object
 sesh = tf.Session()
+# Define variables with initializer
 sesh.run(init)
+
+# tensorboard visualization
+# cmd: \TF_Experiments>tensorboard --logdir=. Url:localhost:6006
+File_Writer = tf.summary.FileWriter('C:\\Uni\\Masterstudium\\Masterarbeit\\Python_TensorFlow\\TF_Experiments\\graph'
+                                    , sesh.graph)
+
+# training data
+x_train = [1, 2, 3, 4]
+y_train = [0, -1, -2, -3]
 
 # train the model
 for i in range(1000):
-    sesh.run(train, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]})
+    sesh.run(train, {x: x_train, y: y_train})
 
-print(sesh.run([W, b]))
+# evaluate accuracy
+curr_W, curr_b, curr_loss = sesh.run([W, b, loss], {x: x_train, y : y_train})
+
+print("W: %s b: %s loss: %s" % (curr_W, curr_b, curr_loss));
 # print(sesh.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
 # print(b);
 
