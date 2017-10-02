8af2c8668ee28451406444f73f6e6ebe28e05638
Alex Werthmann <werdy@gmx.net>
2017-10-02 12:24:09 +0200

    applied code formatting

--- Tensorflow Project/lol.py
+++ Tensorflow Project/lol.py
@@ -1,2 +1,10 @@
 import tensorflow as tf
 
+f = open("..\..\simple-examples\data\ptb.test.txt", 'r')
+i = 0;
+for line in f:
+    if i < 50:
+        print(str(i) + ": " + line)
+        i = i + 1
+
+f.close();
--- Tensorflow Project/reader.py
+++ Tensorflow Project/reader.py
@@ -27,12 +27,13 @@ import tensorflow as tf
 
 Py3 = sys.version_info[0] == 3
 
+
 def _read_words(filename):
-  with tf.gfile.GFile(filename, "r") as f:
-    if Py3:
-        return f.read().replace("\n", "<eos>").split()
-    else:
-        return f.read().decode("utf-8").replace("\n", "<eos>").split()
+    with tf.gfile.GFile(filename, "r") as f:
+        if Py3:
+            return f.read().replace("\n", "<eos>").split()
+        else:
+            return f.read().decode("utf-8").replace("\n", "<eos>").split()
 
 
 def _build_vocab(filename):
@@ -83,44 +84,44 @@ def ptb_raw_data(data_path=None):
 
 
 def ptb_producer(raw_data, batch_size, num_steps, name=None):
-  """Iterate on the raw PTB data.
+    """Iterate on the raw PTB data.
 
-  This chunks up raw_data into batches of examples and returns Tensors that
-  are drawn from these batches.
+    This chunks up raw_data into batches of examples and returns Tensors that
+    are drawn from these batches.
 
-  Args:
-    raw_data: one of the raw data outputs from ptb_raw_data.
-    batch_size: int, the batch size.
-    num_steps: int, the number of unrolls.
-    name: the name of this operation (optional).
+    Args:
+      raw_data: one of the raw data outputs from ptb_raw_data.
+      batch_size: int, the batch size.
+      num_steps: int, the number of unrolls.
+      name: the name of this operation (optional).
 
-  Returns:
-    A pair of Tensors, each shaped [batch_size, num_steps]. The second element
-    of the tuple is the same data time-shifted to the right by one.
+    Returns:
+      A pair of Tensors, each shaped [batch_size, num_steps]. The second element
+      of the tuple is the same data time-shifted to the right by one.
 
-  Raises:
-    tf.errors.InvalidArgumentError: if batch_size or num_steps are too high.
-  """
-  with tf.name_scope(name, "PTBProducer", [raw_data, batch_size, num_steps]):
-      raw_data = tf.convert_to_tensor(raw_data, name="raw_data", dtype=tf.int32)
+    Raises:
+      tf.errors.InvalidArgumentError: if batch_size or num_steps are too high.
+    """
+    with tf.name_scope(name, "PTBProducer", [raw_data, batch_size, num_steps]):
+        raw_data = tf.convert_to_tensor(raw_data, name="raw_data", dtype=tf.int32)
 
-      data_len = tf.size(raw_data)
-      batch_len = data_len // batch_size
-      data = tf.reshape(raw_data[0 : batch_size * batch_len],
-                        [batch_size, batch_len])
+        data_len = tf.size(raw_data)
+        batch_len = data_len // batch_size
+        data = tf.reshape(raw_data[0: batch_size * batch_len],
+                          [batch_size, batch_len])
+        print(str(data_len))
+        epoch_size = (batch_len - 1) // num_steps
+        assertion = tf.assert_positive(
+            epoch_size,
+            message="epoch_size == 0, decrease batch_size or num_steps")
+        with tf.control_dependencies([assertion]):
+            epoch_size = tf.identity(epoch_size, name="epoch_size")
 
-      epoch_size = (batch_len - 1) // num_steps
-      assertion = tf.assert_positive(
-          epoch_size,
-          message="epoch_size == 0, decrease batch_size or num_steps")
-      with tf.control_dependencies([assertion]):
-          epoch_size = tf.identity(epoch_size, name="epoch_size")
-
-      i = tf.train.range_input_producer(epoch_size, shuffle=False).dequeue()
-      x = tf.strided_slice(data, [0, i * num_steps],
-                           [batch_size, (i + 1) * num_steps])
-      x.set_shape([batch_size, num_steps])
-      y = tf.strided_slice(data, [0, i * num_steps + 1],
-                           [batch_size, (i + 1) * num_steps + 1])
-      y.set_shape([batch_size, num_steps])
-      return x, y
+        i = tf.train.range_input_producer(epoch_size, shuffle=False).dequeue()
+        x = tf.strided_slice(data, [0, i * num_steps],
+                             [batch_size, (i + 1) * num_steps])
+        x.set_shape([batch_size, num_steps])
+        y = tf.strided_slice(data, [0, i * num_steps + 1],
+                             [batch_size, (i + 1) * num_steps + 1])
+        y.set_shape([batch_size, num_steps])
+        return x, y
