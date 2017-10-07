a92648535e1ee491baa8d0e00747884d71401534
Alex Werthmann <werdy@gmx.net>
2017-10-07 16:58:28 +0200

    fix

--- Tensorflow Project/lol.py
+++ Tensorflow Project/lol.py
@@ -1,3 +1,7 @@
+import tensorflow as tf
+from tensorflow.contrib.training.python.training.resample import resample_at_rate
+
+
 def test():
     f = open("..\ANN_DATA\simple-examples\data\ptb.test.txt", 'r')
     i = 0;
@@ -22,6 +26,25 @@ def main():
     f.close()
     return myset,i
 
+
+x1 = tf.constant(5)
+x2 = tf.constant(6)
+# From the tensorflow 1.0.0 release notes:
+# tf.mul, tf.sub and tf.neg are deprecated
+# in favor of tf.multiply, tf.subtract and tf.negative.﻿
+result = tf.multiply(x1, x2)
+print(result)
+# Variante 1:
+# sesh = tf.Session()
+# print(sesh.run(result))
+# sesh.close()
+
+# Better:  (automically closes session)
+with tf.Session() as sesh:
+    output = sesh.run(result)
+    print(output)
+
+print(output)
 
 
 
