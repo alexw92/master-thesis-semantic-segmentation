1d8a5cbf35e95279cfbb27b5541b77b4aac2185a
Alex Werthmann <werdy@gmx.net>
2017-12-13 14:01:52 +0100

    added sklearn

--- Map_Stuff/Kaggle_Data_Processor.py
+++ Map_Stuff/Kaggle_Data_Processor.py
@@ -8,6 +8,14 @@ import shapely.affinity
 import cv2
 import numpy as np
 
+## modules for making predictions with sklearn
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import average_precision_score
+
+
 # fixed error _csv.Error: field larger than field limit (131072) in csv.reader(open('...'))
 csv.field_size_limit(2147483647)  # sys.maxsize seems to be to big
 
@@ -21,7 +29,7 @@ if env == 'PC2':
 elif env == 'Main':
     prefix = 'G:/'
 
-image_path = prefix+'ThreeBand_Images'  # 6010_0_0.tif
+image_path = prefix+'ThreeBand_Images'  # RGB 11bit each channel
 grid_sizes = '../ANN_DATA/DSTL SIFD/grid_sizes.csv'
 train_polys = '../ANN_DATA/DSTL SIFD/train_wkt_v4.csv'
 sample_imgid = '6120_2_2'
@@ -96,7 +104,7 @@ def __show_mask(m):
 
 #  data processing tutorial from kaggle
 #  https://www.kaggle.com/c/dstl-satellite-imagery-feature-detection#data-processing-tutorial
-class_name = 'TRACK'
+class_name = 'BUILDING'
 xstart, xend = 2900, 3200
 ystart, yend = 2000, 2300
 
@@ -105,7 +113,6 @@ train_polys = __load_polygons(sample_imgid, PolygonType[class_name])
 
 
 im_rgb = gdal.Open(image_path+"/"+sample_imgid+".tif").ReadAsArray().transpose([1, 2, 0])
-
 imsize = im_rgb.shape[:2]  # first two elements width and height
 x_scaler, y_scaler = __get_scalers(imsize)
 
@@ -127,4 +134,26 @@ plt.title('image: \''+sample_imgid+'\''+' Class = '+class_name)
 plt.xlabel(str(xstart)+' - '+str(xend))
 plt.ylabel(str(ystart)+' - '+str(yend))
 plt.imshow(train_mask, cmap='Greys')
+# plt.show()
+
+# learning with sk learn
+print(np.shape(img))
+print(np.shape(train_mask))
+xs = img.reshape(-1, 3).astype(np.float32)
+ys = train_mask.reshape(-1)
+print(np.shape(xs))
+print(np.shape(ys))
+pipeline = make_pipeline(StandardScaler(), SGDClassifier(loss='log'))
+
+print('training...')
+# do not care about overfitting here
+pipeline.fit(xs, ys)
+pred_ys = pipeline.predict_proba(xs)[:, 1]
+print('average precision', average_precision_score(ys, pred_ys))
+pred_mask = pred_ys.reshape(train_mask.shape)
+
+threshold = 0.5
+pred_mask_bin = pred_mask >= threshold
+
+plt.imshow(pred_mask_bin, cmap='Greys')
 plt.show()
